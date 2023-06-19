5bfec2783776c2d9e1fee6d6cca487f33446fc53
Paul <lazar_paul_2006@yahoo.com>
2023-06-19 21:40:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSharpApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            int triangleBase = 5, height = 6;
+            double triangleArea = 0;
+            triangleArea = 0.5 * (triangleBase * height);
+            
+            Console.WriteLine("Hello there!");
+            Console.WriteLine("The value of PI is:" + pi);
+            Console.WriteLine("The area of this triangle is: " + triangleArea);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
